932c3f74f2031eb6202924771bd178239ef46f19
Saatvik <tikoosaatvik@gmail.com>
2020-01-13 14:53:21 -0800

    File name changed

--- charts.py
+++ charts.py
@@ -1,4 +1,4 @@
-import macroscope
+import macroscore
 
 def clean_data_local():
     # Get required fields of the data-set (fields not of the Replication Study)
@@ -7,25 +7,25 @@ def clean_data_local():
                       'Calculated P-value (O)', 'Test statistic (O)', 'Effect size (O)', 'Type of analysis (O)']
     data_types = ['Integer', 'Range', 'Categorical', 'Decimal', 'Dichotomous', 'Open text']
     with open("Psychology/rpp_data_codebook.csv") as input_file:
-        reader = macroscope.csv.reader(input_file)
+        reader = macroscore.csv.reader(input_file)
         # skip the header
         next(reader)
         for line in reader:
             if '(R)' not in line[0] and line[4] in data_types and line[0] not in remove_columns:
                 req_columns.append(line[0])
-    data_set = macroscope.pandas.read_csv('Psychology/rpp_data_updates.csv', encoding='ansi', usecols=req_columns)
+    data_set = macroscore.pandas.read_csv('Psychology/rpp_data_updates.csv', encoding='ansi', usecols=req_columns)
 
     # Add output field and dropping the Replication fields
-    data_set['result_label'] = data_set.apply(lambda row: macroscope.label_addition(row), axis=1)
+    data_set['result_label'] = data_set.apply(lambda row: macroscore.label_addition(row), axis=1)
     data_set = data_set.drop(['P-value (R)', 'Direction (R)'], axis=1)
 
     # Clean fields
-    data_set = data_set.replace(to_replace='X', value=macroscope.np.nan)
-    data_set = data_set.replace(to_replace=macroscope.np.nan, value=0)
-    data_set = macroscope.clean_pvalue(data_set, 'Reported P-value (O)')
-    data_set = macroscope.clean_range_values(data_set, 'Pages (O)')
-    data_set = macroscope.clean_float_result(data_set, 'Surprising result (O)')
-    data_set = macroscope.clean_float_result(data_set, 'Exciting result (O)')
+    data_set = data_set.replace(to_replace='X', value=macroscore.np.nan)
+    data_set = data_set.replace(to_replace=macroscore.np.nan, value=0)
+    data_set = macroscore.clean_pvalue(data_set, 'Reported P-value (O)')
+    data_set = macroscore.clean_range_values(data_set, 'Pages (O)')
+    data_set = macroscore.clean_float_result(data_set, 'Surprising result (O)')
+    data_set = macroscore.clean_float_result(data_set, 'Exciting result (O)')
 
     # Removing the last row as it is a null row
     data_set = data_set[: -1]
